d5d8ec6232b2d0d622efd77f0d116d6eb10cda04
Maxime Tournier <maxime.tournier@inovatel.com>
2019-09-09 18:22:11 +0200

    [App] Add some modules + error handling

--- app/app.ts
+++ app/app.ts
@@ -1,42 +1,87 @@
-// var bodyParser = require("body-parser");
+// app.ts
+/**
+ * Heart of the Amok server, activate middlewares and start the back
+ */
+import express = require('express');
+import bodyParser = require('body-parser');
+import errorhandler = require('errorhandler');
+import mongoose = require('mongoose');
 
-// var isProduction = process.env.NODE_ENV === 'production';
+var isProduction = process.env.NODE_ENV === "production";
 
-// var app = express();
+const app: express.Application = express();
 
 // // morgan is an express middleware wich log http request
 // // dev seems to auto-reload the run when a file has changed. To be confirmed
-// app.use(require('morgan')('dev'));
+app.use(require('morgan')('dev'));
 
 // // body-parser is used to pre-parse request before entering in the handler
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // // lets you use HTTP verbs such as PUT or DELETE in places where the client doesn't support it
 // // no sure we will use it
-// app.use(require('method-override')());
+app.use(require('method-override')());
+
 // // define path for public static asset such as css or image
-// app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
-// // oh! pretty sure this one launch the server ;)
-// var server = app.listen( process.env.PORT || 3000, function() {
-//     console.log('Listening on port ' + server.address().port);
-//   });
+// display more errors in logs
+if (!isProduction) {
+  app.use(errorhandler());
+}
 
-  // lib/app.ts
-import express = require('express');
-import bodyParser = require('body-parser')
-
-// Create a new express application instance
-const app: express.Application = express();
+// for mongoDB
+if(isProduction){
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  // mongoose.connect('mongodb://localhost/conduit');
+  mongoose.set('debug', true);
+}
 
 app.get('/', function (req, res) {
-  res.send('Hello World! 2');
+  res.send('Hello World! 3');
 });
 
-app.listen(3000, function () {
+// Add some require('./PATH/FILE_NAME')
+
+//app.use(require('./routes/index'));
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (!isProduction) {
+  app.use(function(err, req, res, next) {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({'errors': {
+      message: err.message,
+      error: err
+    }});
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({'errors': {
+    message: err.message,
+    error: {}
+  }});
+});
+
+// // oh! pretty sure this one launch the server ;)
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
-
-// npm run tsc
-// node build/app.js
